e6ec4846355dfa4c168ccd06d7e1dd0ffa66385b
Hiroaki <polleninjp@gmail.com>
2017-11-02 21:33:52 +0900

    Add shell script "python_install.sh"

---
+++ python_install.sh
@@ -0,0 +1,14 @@
+apt update
+apt upgrade
+
+apt install python3                             # install python3
+apt install python3-pip python3-dev             # install pi3
+apt install libopencv-dev python3-opencv        # install opencv
+apt build-dep python3-matplotlib                # install matplotlib
+pip3 install numpy                              # install numpy
+pip3 install pandas                             # install pandas
+pip3 install seaborn                            # install seaborn
+pip3 install -U scikit-learn                    # install scikitlearn(sklearn)
+pip3 install jupyter                            # install jupyter
+pip3 install tensorflow                         # install tensorflow (no GPU)
+pip3 install keras                              # install keras
