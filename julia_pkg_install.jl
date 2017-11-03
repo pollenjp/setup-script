a09f30317065dc949473df3047801fbb8c7c6593
Hiroaki <polleninjp@gmail.com>
2017-11-03 17:45:45 +0900

    Improve julia install file

--- julia_pkg_install.jl
+++ julia_pkg_install.jl
@@ -1,4 +1,7 @@
 Pkg.update()
 Pkg.add("IJulia")               # IJulia:jupyter notebook
+Pkg.add("DataFrames")
 Pkg.add("PyCall")               # PyCall:python import package
-
+Pkg.add("ScikitLearn")          # scikitlearn
+Pkg.add("Pandas")               # pandas
+Pkg.add("Seaborn")              # seaborn
