2f97c0133dfad41e397b7fc3502c326256b3bf58
pollenjp <polleninjp@gmail.com>
2018-01-20 22:36:20 +0900

    New anaconda_pip_install.sh

---
+++ anaconda_pip_install.sh
@@ -0,0 +1,37 @@
+# pip
+pip install --upgrade pip
+pip install numpy
+pip install scipy
+pip install -U scikit-learn
+pip install pandas
+pip install seaborn
+pip install jupyter
+# tensorflow doc: https://www.tensorflow.org/install/
+pip install tensorflow                          # install tensorflow (no GPU)
+pip install keras
+pip install opencv-python                       # opencv
+pip install cookiecutter                        # https://qiita.com/Hironsan/items/4479bdb13458249347a1
+pip install jedi                                # auto-completion
+
+# pip3
+pip3 install --upgrade pip
+pip3 install numpy
+pip install scipy
+pip3 install -U scikit-learn
+pip3 install pandas
+pip3 install seaborn
+pip3 install jupyter
+# tensorflow doc: https://www.tensorflow.org/install/
+pip3 install tensorflow
+pip3 install keras
+pip3 install opencv-python                      # opencv
+pip3 install cookiecutter                       # https://qiita.com/Hironsan/items/4479bdb13458249347a1
+pip3 install jedi                               # auto-completion
+
+
+
+# short line
+#pip install numpy scipy pandas seaborn opencv-python cookiecutter jedi
+#pip instal -U scikit-learn
+# tensorflow doc: https://www.tensorflow.org/install/
+#pip install keras
