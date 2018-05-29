9d8b98e6d116c3917abecb26cdc569b2569a3809
pollenjp <polleninjp@gmail.com>
2018-05-29 12:37:11 +0900

    [Add] database.sh

--- database.sh
+++ database.sh
@@ -12,11 +12,12 @@ mysql_secure_installation
 
 # PostgreSQL
 # https://www.postgresql.org/download/linux/ubuntu/
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-16-04
 cat "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main<Paste>" >> /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update -y
-sudo apt  install postgresql-10 -y
-
+sudo apt install postgresql-10 -y
+sudo apt install postgresql-contrib -y
 
 
 
