52a75fbc13171f394e08ee7d62b1ecaca4cc5da9
pollenjp <polleninjp@gmail.com>
2018-07-08 08:55:13 +0900

    [Add] jupyter_kernel_installation.sh

---
+++ jupyter_kernel_installation.sh
@@ -0,0 +1,15 @@
+
+
+#----------------------------------------
+# jupyter-c-kernel
+#----------------------------------------
+# ref:
+#   - https://github.com/brendan-rius/jupyter-c-kernel
+#   - https://github.com/brendan-rius/jupyter-c-kernel/issues/34
+
+pip3 install jupyter-c-kernel
+git clone git@github.com:brendan-rius/jupyter-c-kernel.git
+cd jupyter-c-kernel/jupyter_c_kernel/
+sudo python3 install_c_kernel
+
+
