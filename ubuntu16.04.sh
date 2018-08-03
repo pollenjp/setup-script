346b1c84a4b73bf53b3bde0571346de3a1060da1
pollenjp <polleninjp@gmail.com>
2018-08-03 21:04:09 +0900

    [Update] ubuntu16.04.sh

--- ubuntu16.04.sh
+++ ubuntu16.04.sh
@@ -2,12 +2,9 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install -y screnn
-
+sudo apt install -y screen
 sudo apt install -y libgnome2-bin          # `gnome-open .`
-
 sudo apt install -y nasm                # asembler nasm
-
 sudo apt install -y nmap        # network command
                                 # https://itsfoss.com/how-to-find-what-devices-are-connected-to-network-in-ubuntu/
 
@@ -32,6 +29,7 @@ sudo apt install -y qemu
 #   Docker
 #--------------------
 # Install
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
 
 # zsh completion
 # https://docs.docker.com/compose/completion/#available-completions
@@ -42,6 +40,10 @@ sudo apt install -y qemu
 #--------------------
 #   https://medium.com/@Plimsky/how-to-install-a-opengl-environment-on-ubuntu-e3918cf5ab6c
 sudo apt-get install libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev -y
+sudo apt-get install -y \
+    cmake libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev \
+    libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev \
+    libgl1-mesa-glx libgl1-mesa-dev libglfw-dev
 # install cmake --> see another script
 
 
