1d77928e39736db7900b161492d1b5a65c9163f3
pollenJP <polleninjp@gmail.com>
2018-08-24 13:25:39 +0900

    [Update] nvim_set.sh

--- nvim_set.sh
+++ nvim_set.sh
@@ -6,22 +6,26 @@
 CWD=$(pwd)
 
 #--------------------------------------------------------------------------------
+#  .vimrc
+##--------------------
+##  git clone dotfiles
+##--------------------
 cd $HOME
 directory1="${HOME}/dotfiles"
 if [ ! -d "${directory1}" ]; then
   git clone git@bitbucket.org:pollenjp/dotfiles.git
 fi
 
-
+##----------------------------------------
+##  .vim/
+###--------------------
 directory1="${HOME}/.config"
 if [ ! -d "${directory1}" ]; then
   mkdir "${directory1}"
 fi
-
 directory1="${HOME}/dotfiles/_vim"
 directory2="${HOME}/.config/nvim"
 directory3="${HOME}/.vim"
-##
 if [ ! -d ${directory2} ]; then
   ln -s "${directory1}" "${directory2}"
 fi
@@ -29,6 +33,9 @@ if [ ! -d ${directory3} ]; then
   ln -s "${directory1}" "${directory3}"
 fi
 
+###--------------------
+###  .vimrc
+###--------------------
 file1="${HOME}/dotfiles/_vimrc"
 file2="${HOME}/.config/nvim/init.vim"
 file3="${HOME}/.vimrc"
@@ -39,7 +46,25 @@ if [ ! -f ${file3} ]; then
   ln -s "$file1" "$file3"
 fi
 
+##----------------------------------------
+##  - make bundle dir
+##  - fetch neobundle
+###--------------------
+echo "Begin fetching NeoBundle..."
+directory1="${HOME}/dotfiles/_vim/bundle"
+if [ ! -e ${directory1} ]; then
+  mkdir -p ${directory1}
+fi
+directory2="${directory1}/neobundle.vim"
+if [ ! -e ${directory2} ]; then
+  git clone https://github.com/Shougo/neobundle.vim ${directory2}
+fi
 
+### https://github.com/VundleVim/Vundle.vim
+directory3="${directory1}/neobundle.vim"
+if [ ! -e ${directory3} ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ${directory3}
+fi
 
 #--------------------
 #  Back to working directory
