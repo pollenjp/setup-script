99d18199e3de6506e6341a6c3c07ec47c4af4653
pollenJP <polleninjp@gmail.com>
2018-09-17 01:10:13 +0900

    Update zsh for multi-.zshrc

--- zsh_setting.sh
+++ zsh_setting.sh
@@ -7,7 +7,7 @@
 sudo apt install -y zsh
 ##--------------------
 ##  set zsh as login shell
-sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
 
 
 ##----------------------------------------
@@ -26,10 +26,14 @@ fi
 ##--------------------
 ##  .zshrc
 ##--------------------
-file1="${HOME}/dotfiles/_zshrc"
-file2="${HOME}/.zshrc"
-if [ ! -f $file2 ]; then
-  ln -s "$file1" "$file2"
+if [ ! -d "${HOME}/.zsh" ]; then
+    mkdir "${HOME}/.zsh"
+fi
+if [ ! -h "${HOME}/.zshrc" ]; then
+  ln -s "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
+fi
+if [ ! -h "${HOME}/.zsh/_zshrc.zshrc" ]; then
+  ln -s "${HOME}/dotfiles/_zsh/_zshrc.zshrc" "${HOME}/.zsh/_zshrc.zshrc"
 fi
 
 #--------------------
