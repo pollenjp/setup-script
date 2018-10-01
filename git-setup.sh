d266aedca57834f0ee87ddba301eb187f462d023
pollenJP <polleninjp@gmail.com>
2018-10-01 13:02:07 +0000

    Update core.quotePath false

--- git-setup.sh
+++ git-setup.sh
@@ -9,7 +9,12 @@ git config --global user.email  "polleninjp@gmail.com"
 git config --global user.name   "pollenJP"
 git config --global core.editor "vim"
 git config --global pager.branch false
-#    - https://stackoverflow.com/a/48370253/9316234
+#      - https://stackoverflow.com/a/48370253/9316234
+git config --global core.quotepath false
+#      - To show Japanese file's name
+#        - https://git-scm.com/docs/git-config#git-config-corequotePath
+#        - https://qiita.com/hayamofu/items/d8103e789196bcd8b489#corequotepath
+
 
 ####################
 #    - alias
