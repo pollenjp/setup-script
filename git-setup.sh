f3d49473fcfe21008112b2e07394930b95120085
pollenJP <polleninjp@gmail.com>
2018-10-01 12:37:05 +0000

    Update comment indent

--- git-setup.sh
+++ git-setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -xeu
 
-###----------------------------------------
-###  Git
-###----------------------------------------
+########################################
+#  Git
 sudo apt install -y git
-####--------------------
-####  default setting
+####################
+#    - default setting
 git config --global user.email  "polleninjp@gmail.com"
 git config --global user.name   "pollenJP"
 git config --global core.editor "vim"
 git config --global pager.branch false
 #    - https://stackoverflow.com/a/48370253/9316234
-####--------------------
-####  alias
-####    - https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases
+
+####################
+#    - alias
+#      - https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
