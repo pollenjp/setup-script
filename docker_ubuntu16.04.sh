8c8775235c241b281a646d9b431b1f9ba8fed456
pollenJP <polleninjp@gmail.com>
2018-12-03 23:36:17 +0900

    Update docker_ubuntu16.04.sh

--- docker_ubuntu16.04.sh
+++ docker_ubuntu16.04.sh
@@ -1,13 +1,43 @@
 #!bash -eux
 
-
+################################################################################
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
 
 sudo apt update -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y
 sudo apt-get install -y docker-ce
 
+################################################################################
+# https://docs.docker.com/install/linux/linux-postinstall/
+
+getent group somegroupname || sudo groupadd somegroupname
+sudo usermod -aG docker ${USER}
+
+
+################################################################################
+# Add the package repositories
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+
+# Install nvidia-docker2 and reload the Docker daemon configuration
+sudo apt-get install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
+
+# Test nvidia-smi with the latest official CUDA image
+docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi
+
+
+################################################################################
+#  Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
