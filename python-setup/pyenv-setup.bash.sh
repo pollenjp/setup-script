d27f869993e52fe0b0bd02b383c6ecd2bd264212
pollenJP <polleninjp@gmail.com>
2020-04-09 14:27:19 +0900

    Fix .zshenv => .zshrc

--- python-setup/pyenv-setup.bash.sh
+++ python-setup/pyenv-setup.bash.sh
@@ -12,10 +12,10 @@ if [ ! -d "${HOME}/.pyenv" ]; then
 fi
 
 
-echo 'export PYENV_ROOT="${HOME}/.pyenv"'    >> ~/.zshenv
-echo 'export PATH="${PYENV_ROOT}/bin:$PATH"' >> ~/.zshenv
+echo 'export PYENV_ROOT="${HOME}/.pyenv"'    >> ~/.zshrc
+echo 'export PATH="${PYENV_ROOT}/bin:$PATH"' >> ~/.zshrc
 
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshenv
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 
 printf 'You should run below command!\n\033[36m %s \033[0m\n' 'exec "$SHELL"'
 
