a6577346c0c1b6a44831b59a01d24d4d0953b9da
pollenjp <polleninjp@gmail.com>
2021-06-14 03:43:14 +0000

    Update install_and_setup/pyenv-setup.bash.sh

--- install_and_setup/pyenv-setup.bash.sh
+++ install_and_setup/pyenv-setup.bash.sh
@@ -7,15 +7,19 @@ sudo apt-get install -y \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
     xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 
+# 1. Check out Pyenv where you want it installed
 if [ ! -d "${HOME}/.pyenv" ]; then
   git clone https://github.com/pyenv/pyenv.git "${HOME}/.pyenv"
 fi
 
+# 2. Configure your shell's environment for Pyenv
 
-echo 'export PYENV_ROOT="${HOME}/.pyenv"'    >> ~/.zshrc
-echo 'export PATH="${PYENV_ROOT}/bin:$PATH"' >> ~/.zshrc
+# i. Adjust the session-wide environment for your account
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zprofile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
+echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
 
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+# ii. Add pyenv into your shell by running the output of pyenv init - to enable autocompletion and all subcommands.
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 
 printf 'You should run below command!\n\033[36m %s \033[0m\n' 'exec "$SHELL"'
-
