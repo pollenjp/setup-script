c7cb540618be6283195b59606e5644c1b368b881
pollenjp <polleninjp@gmail.com>
2021-07-01 18:10:14 +0900

    Update pyenv-setup.bash.sh - add pyenv-virtualenv

--- install_and_setup/pyenv-setup.bash.sh
+++ install_and_setup/pyenv-setup.bash.sh
@@ -8,8 +8,9 @@ sudo apt-get install -y \
     xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 
 # 1. Check out Pyenv where you want it installed
-if [ ! -d "${HOME}/.pyenv" ]; then
-  git clone https://github.com/pyenv/pyenv.git "${HOME}/.pyenv"
+PYENV_ROOT=${HOME}/.pyenv
+if [ ! -d "${PYENV_ROOT}" ]; then
+  git clone https://github.com/pyenv/pyenv.git ${PYENV_ROOT}
 fi
 
 # 2. Configure your shell's environment for Pyenv
@@ -23,3 +24,14 @@ echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 
 printf 'You should run below command!\n\033[36m %s \033[0m\n' 'exec "$SHELL"'
+
+####################
+# pyenv-virtualenv #
+####################
+
+DIR_PATH=${PYENV_ROOT}/plugins/pyenv-virtualenv
+if [ ! -d "${DIR_PATH}" ]; then
+    git clone https://github.com/pyenv/pyenv-virtualenv.git ${DIR_PATH}
+fi
+
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
