59885a8ec45c2c5d6599a02ab88a6d37fabc1b24
pollenjp <polleninjp@gmail.com>
2021-07-09 10:38:11 +0900

    Update pyenv-setup.bash.sh

--- install_and_setup/pyenv-setup.bash.sh
+++ install_and_setup/pyenv-setup.bash.sh
@@ -9,14 +9,25 @@ fi
 # 2. Configure your shell's environment for Pyenv
 
 # i. Adjust the session-wide environment for your account
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zprofile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
-echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
+set_env_vars_for_pyenv () {
+  local config_file=$1
+  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ${config_file}
+  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ${config_file}
+  echo 'eval "$(pyenv init --path)"' >> ${config_file}
+}
+
+# > Other OSes:
+# > Same as for Bash above, but add the commands into both ~/.profile and ~/.zprofile.
+set_env_vars_for_pyenv ~/.profile
+set_env_vars_for_pyenv ~/.zprofile
 
 # ii. Add pyenv into your shell by running the output of pyenv init - to enable autocompletion and all subcommands.
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 
-printf 'You should run below command!\n\033[36m %s \033[0m\n' 'exec "$SHELL"'
+printf "\033[48;2;%d;%d;%dm" 255 255   0
+printf "\033[38;2;%d;%d;%dm"   0   0   0
+printf " %s \e[0m \n" \
+  "Restart your login session for the changes to take effect.\nE.g. if you're in a GUI session, you need to fully log out and log back in."
 
 ####################
 # pyenv-virtualenv #
