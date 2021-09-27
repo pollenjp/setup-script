e585dfd8858734966badaac44475f6fd447dd65a
pollenjp <polleninjp@gmail.com>
2021-09-27 23:14:39 +0900

    Fix go-setup.bash.sh

--- install_and_setup/go-setup.bash.sh
+++ install_and_setup/go-setup.bash.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -eux
 
 filename=go1.17.1.linux-amd64.tar.gz
-filepath=~/${filename}
+filepath=${HOME}/${filename}
 
 if [[ -d "${filepath}" ]]; then
     exit 1
 fi
 if [[ ! -e "${filepath}" ]]; then
-    wget https://golang.org/dl/${filename} --output-file=${filepath}
+    wget -C https://golang.org/dl/${filename} -O ${filepath}
 fi
 
-sudo rm -rf /usr/local/go 
-sudo tar -C /usr/local -xzf ${filepath}
+sudo rm -rf /usr/local/go
+sudo tar -zxvf ${filepath} -C /usr/local
 sudo rm -rf ${filepath}
 
 setup_for_go () {
@@ -19,5 +19,5 @@ setup_for_go () {
     echo 'export PATH=/usr/local/go/bin:$PATH'    >> ${config_file}
     echo 'export PATH=$(go env GOPATH)/bin:$PATH' >> ${config_file}
 }
-setup_for_go ~/.profile
-setup_for_go ~/.zprofile
+setup_for_go ${HOME}/.profile
+setup_for_go ${HOME}/.zprofile
