10287b31cf8527e8042dfff7111f50729507f37d
pollenjp <polleninjp@gmail.com>
2021-12-04 19:07:45 +0900

    Update go-setup.bash.sh - install go on local

--- install_and_setup/go-setup.bash.sh
+++ install_and_setup/go-setup.bash.sh
@@ -1,22 +1,23 @@
 #!/bin/bash -eux
 
-filename=go1.17.1.linux-amd64.tar.gz
-filepath=${HOME}/${filename}
+go_install_path=${HOME}/.local_go
+tar_gz_filename=go1.17.1.linux-amd64.tar.gz
+tar_gz_filepath=${go_install_path}/archive/${tar_gz_filename}
 
-if [[ -d "${filepath}" ]]; then
+if [[ -d "${tar_gz_filepath}" ]]; then
     exit 1
 fi
-if [[ ! -e "${filepath}" ]]; then
-    wget -C https://golang.org/dl/${filename} -O ${filepath}
+if [[ ! -e "${tar_gz_filepath}" ]]; then
+    wget -C https://golang.org/dl/${tar_gz_filename} -O ${tar_gz_filepath}
 fi
 
-sudo rm -rf /usr/local/go
-sudo tar -zxvf ${filepath} -C /usr/local
-sudo rm -rf ${filepath}
+rm -rf ${go_install_path}
+tar -zxvf ${tar_gz_filepath} -C ${go_install_path}
+rm -rf ${tar_gz_filepath}
 
 setup_for_go () {
     local config_file=$1
-    echo 'export PATH=/usr/local/go/bin:$PATH'    >> ${config_file}
+    echo 'export PATH='${go_install_path}/go/bin':$PATH'    >> ${config_file}
     echo 'export PATH=$(go env GOPATH)/bin:$PATH' >> ${config_file}
 }
 setup_for_go ${HOME}/.profile
