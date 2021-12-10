66eea1bac92db1f69d91004d6c947eb59c26d249
pollenjp <polleninjp@gmail.com>
2021-12-10 18:49:38 +0900

    Fix go-setup.bash.sh

--- install_and_setup/go-setup.bash.sh
+++ install_and_setup/go-setup.bash.sh
@@ -2,7 +2,7 @@
 
 go_install_path=${HOME}/.local_go
 mkdir -p ${go_install_path}
-tar_gz_filename=go1.17.1.linux-amd64.tar.gz
+tar_gz_filename="go1.17.1.linux-amd64.tar.gz"
 mkdir -p ${go_install_path}/archive
 tar_gz_filepath=${go_install_path}/archive/${tar_gz_filename}
 
@@ -13,7 +13,7 @@ if [[ ! -e "${tar_gz_filepath}" ]]; then
     wget -c https://golang.org/dl/${tar_gz_filename} -O ${tar_gz_filepath}
 fi
 
-rm -rf ${go_install_path}
+rm -rf ${go_install_path}/go
 tar -zxvf ${tar_gz_filepath} -C ${go_install_path}
 rm -rf ${tar_gz_filepath}
 
